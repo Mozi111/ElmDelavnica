92dcf1328ad9657719ec1510cf4a6e230a9c574f
Mozi111 <SplinterCell.Savage@gmail.com>
2019-12-18 17:43:13 +0100

    dodali uporabnike

--- src/App.elm
+++ src/App.elm
@@ -1,4 +1,4 @@
-module App exposing(..)
+port module App exposing(..)
 
 import Html
 import Html.Events as He
@@ -14,20 +14,36 @@ type alias Model = {
     users: List User
     }
 
-type Msg = Initialize | Increment | Decrement
+type Msg = Initialize | Increment | Decrement | DeleteUser User --| EditUserMsg User EditUser
+
+type Page = LoginPage | GroupPage | MainPage
+type EditUser = Delete | IncPoints | DecPoints
 
 intialModel : () -> (Model, Cmd Msg)
 intialModel () = ({value=0, numberOfClicks=0, users=initialUsers}, Cmd.none)
 
 view model =
-    Grid.container [] [
+    Grid.container [] ([
         Html.h1 [] [Html.text "Nejc je na delavnici s Tjaso!\n In kako je ustvarjalen! Nejc namreč."], --"Hello world"
         Html.h2 [] [Html.text ((String.fromInt model.value) ++ " je najino najljubse stevilo")],
         Html.h2 [] [Html.text ((String.fromInt model.numberOfClicks) ++ "-krat sva si premislila")],
         Button.button [Button.success, Button.block, Button.attrs [He.onClick Increment] ] [Html.text "Nejc"],
         Button.button [Button.success, Button.block, Button.attrs [He.onClick Decrement] ] [Html.text "Tjasa"],
-        Button.button [Button.success, Button.block, Button.attrs [He.onClick Initialize] ] [Html.text "Nazaj na zacetek"]
-    ]
+        Button.button [Button.success, Button.block, Button.attrs [He.onClick Initialize] ] [Html.text "Nazaj na zacetek"],
+        Grid.row [] [
+            Grid.col [] [Html.text "Uid"],
+            Grid.col [] [Html.text "Ime"],
+            Grid.col [] [Html.text "Tocke"],
+            Grid.col [] []
+        ]
+    ] ++ (
+        List.map (\user -> Grid.row [] [
+            Grid.col [] [Html.text (String.fromInt user.uid)],
+            Grid.col [] [Html.text user.name],
+            Grid.col [] [Html.text (String.fromInt user.points)],
+            Grid.col [] [Button.button [Button.onClick (DeleteUser user)] [Html.text "Delete"]]
+        ]) (List.reverse (List.sortBy .points model.users)) 
+    ))
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -35,6 +51,7 @@ update msg model =
         Increment -> ({model | value = model.value + 1, numberOfClicks = model.numberOfClicks + 1}, Cmd.none)
         Decrement -> ({model | value = model.value - 1, numberOfClicks = model.numberOfClicks + 1}, Cmd.none)
         Initialize -> intialModel ()
+        DeleteUser user -> ({model | users = remove user model.users}, sendAlert user.name)
         --_ -> (model, Cmd.none)
 
 main = 
@@ -45,3 +62,5 @@ main =
             view = view,
             subscriptions = \_ -> Sub.none
         }
+
+port sendAlert : String -> Cmd msg
--- src/User.elm
+++ src/User.elm
@@ -6,8 +6,20 @@ type alias User = {
     points: Int
     }
 
+remove : User -> List User -> List User
+remove user users = 
+    case users of
+        [] -> []
+        (usr::rest) -> if usr.uid == user.uid then
+                        remove user rest else
+                        usr :: (remove user rest)
+
 initialUsers =
     [
         {uid=1, name="Tjasa", points = 120},
+        {uid=3, name="Marko", points = 12},
+        {uid=4, name="Martin", points = -10},
+        {uid=5, name="Kontejner", points = 19},
         {uid=2, name="Nejc", points = 17}
     ]
+
